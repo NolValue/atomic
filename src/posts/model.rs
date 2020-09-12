3eb122f945fe460886d0a6ce5b2c8b6598f2b502
nolvalue <wolfkingboyasriel@gmail.com>
2020-09-12 07:50:41 -0500

    Further work on post creation. Almost complete. Then code cleanup -> Media Support

--- src/posts/model.rs
+++ src/posts/model.rs
@@ -10,18 +10,22 @@ use rocket::futures::TryFutureExt;
 use rocket::data::ByteUnit;
 use regex::internal::Input;
 use rocket::outcome::Outcome::Success;
-use crate::utils::set_timer_days;
+use crate::utils::{set_timer_days, gen_id};
 use rocket::futures::io::BufWriter;
 use rocket::tokio;
 use std::iter::once;
 use std::convert::Infallible;
+use crate::auth::model::Session;
+use crate::routes::AtomicDB;
+use std::str::FromStr;
 
 pub enum PostError{
     InvalidData,
     TooManyFields,
     TooFewFields,
     WrongType,
-    MissingBoundary
+    MissingBoundary,
+    Unsupported
 }
 #[derive(Queryable, Insertable, Serialize, Deserialize, Clone)]
 pub struct Post {
@@ -43,17 +47,32 @@ impl FromData for Post{
         let cont = request.content_type().unwrap();
         let (_, boundary) = cont.params().find(|&(k, _)| k == "boundary").unwrap();
         let buf = data.open(8.megabytes());
-        let multipart = Multipart::with_reader(buf, boundary);
-        return Success(Post{
-            id: "0515".to_string(),
+        let mut multipart = Multipart::with_reader(buf, boundary);
+        let auth = request.guard::<Session>().await.unwrap();
+        let conn = request.guard::<AtomicDB>().await.unwrap();
+        let mut post = Post{
+            id: gen_id(21),
             source_type: None,
             source_id: None,
             public: None,
             reshares: None,
             comments: None,
-            poster: "1851".to_string(),
-            content: "Your mom".to_string(),
+            poster: auth.get_uid(&*conn),
+            content: "".to_string(),
             created_on: set_timer_days(0)
-        })
+        };
+        while let Some(field) = multipart.next_field().await.unwrap() {
+            let name = field.name();
+            match name{
+                Some("source_type") => post.source_type = Some(i16::from_str(field.text().await.unwrap().as_str()).unwrap()),
+                Some("source_id") => post.source_id = Some(field.text().await.unwrap()),
+                Some("content") => post.content = field.text().await.unwrap(),
+                Some("public") => post.public = Some(bool::from_str(field.text().await.unwrap().as_str()).unwrap()),
+                Some("reshares") => post.reshares = Some(bool::from_str(field.text().await.unwrap().as_str()).unwrap()),
+                Some("comments") => post.comments = Some(bool::from_str(field.text().await.unwrap().as_str()).unwrap()),
+                _ => return Outcome::Failure((Status::BadRequest, PostError::Unsupported))
+            }
+        }
+        return Success(post)
     }
 }
--- src/posts/routes.rs
+++ src/posts/routes.rs
@@ -1,9 +1,9 @@
 use crate::posts::model::Post;
 use crate::routes::AtomicDB;
 use crate::auth::model::Session;
-use rocket_contrib::json::JsonValue;
+use rocket_contrib::json::{JsonValue, Json};
 
 #[post("/post", data = "<post>")]
-pub async fn test(mut post: Post, conn: AtomicDB) -> JsonValue {
-    json!({ "status": "Check Console" })
+pub async fn test(mut post: Post, conn: AtomicDB, auth: Session) -> Json<Post> {
+    Json(post)
 }
